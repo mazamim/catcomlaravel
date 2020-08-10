8b3cbfc5ebce5a08975b8ffb1f45e2dff7f336fa
Mazahim <mazamim91@gmail.com>
2020-08-10 21:20:12 +1000

    1008

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,6 +9,7 @@ use App\Task;
 use App\Attendance;
 use App\Photo;
 use App\MyProject;
+use App\JobType;
 use App\Ticketchild;
 use App\Http\Requests;
 use Illuminate\Support\Facades\DB;
@@ -32,6 +33,76 @@ class ProjectController extends Controller
        // ->simplePaginate(15);
         return response()->json($data,200);
     }
+
+    //Route::get('jobtypes', 'ProjectController@getjobtypes')->middleware("cors");
+
+    public function getjobtypes()
+    {
+
+        $data = DB::table('job_types')
+        ->get();
+
+        return response()->json($data,200);
+    }
+
+
+//Route::get('countJobs', 'ProjectController@countJobs')->middleware("cors");
+
+public function countJobs()
+{
+
+
+
+    $assigned = DB::table('my_projects')
+    ->select(DB::raw('count(*) as x'))
+    ->where('status', '=', 'Assigned')
+    ->get();
+    $area1 = json_encode($assigned, true);
+    $assigned1 = trim($area1,"[{\"x\":}]");
+
+
+    $pending = DB::table('my_projects')
+    ->select(DB::raw('count(*) as x'))
+    ->where('status', '=', 'Pending')
+    ->get();
+    $area2 = json_encode($pending, true);
+    $pending1 = trim($area2,"[{\"x\":}]");
+
+    $notClosed = DB::table('my_projects')
+    ->select(DB::raw('count(*) as x'))
+    ->where('status', '=', 'Completed_not_Closed')
+    ->get();
+    $area3 = json_encode($notClosed, true);
+    $notClosed1 = trim($area3,"[{\"x\":}]");
+
+    $notInvoiced = DB::table('my_projects')
+    ->select(DB::raw('count(*) as x'))
+    ->where('status', '=', 'Completed_not_Invoiced')
+    ->get();
+    $area4 = json_encode($notInvoiced, true);
+    $notInvoiced1 = trim($area4,"[{\"x\":}]");
+
+    $invoiceupdated = DB::table('my_projects')
+    ->select(DB::raw('count(*) as x'))
+    ->where('status', '=', 'Completed_and_Invoice_Updated')
+    ->get();
+    $area5 = json_encode($invoiceupdated, true);
+    $invoiceupdated1 = trim($area5,"[{\"x\":}]");
+
+
+
+    $products1 = collect(['name' => 'Assigned' , 'value' => $assigned1]);
+    $products2 = collect(['name' =>'Pending' , 'value' => $pending1]);
+    $products3 = collect(['name' =>'Completed_not_Closed' , 'value' => $notClosed1 ]);
+    $products4 = collect(['name' => 'Completed_not_Invoiced', 'value' => $notInvoiced1]);
+    $products5 = collect(['name' => 'Completed_and_Invoice_Updated', 'value' => $invoiceupdated1]);
+
+$products=[$products1,$products2,$products3,$products4,$products5];
+    return response()->json($products,200);
+}
+
+
+
     //Route::get('projects8', 'ProjectController@show8')->middleware("cors");
     public function show8()
     {
@@ -69,6 +140,15 @@ class ProjectController extends Controller
         return response()->json($data,201);
     }
 
+    //Route::post('jobtypes', 'ProjectController@addjobtypes')->middleware("cors");
+    public function addjobtypes(Request $request)
+    {
+        $data = new JobType();
+        $data->jobType = $request->input('jobType');
+
+        $data->save();
+        return response()->json($data,201);
+    }
 
    // Route::post('addbulk', 'ProjectController@addbulk')->middleware("cors");
    public function addbulk(Request $request)
--- app/JobType.php
+++ app/JobType.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobType extends Model
 {
-    //
+    protected $table = 'job_types';
+    protected $fillable = ['jobType'];
 }
